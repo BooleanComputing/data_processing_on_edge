559604fe609e7d43bc9618725a7676a624c5c503
Vasista Polali <vasistabhargava@gmail.com>
2023-07-11 13:51:44 +0200

    update_deploy_script

--- deploy-architecture.sh
+++ deploy-architecture.sh
@@ -23,7 +23,7 @@ cp airflow/dags/* /mnt/airflow/dags/
 cp application/*  /mnt/airflow/scripts/
 
 #Bring up Airflow cluster as a Docker Swarm Service
-cd airflow && sudo docker stack deploy -c docker-compose.yml  airflow-cluster
+cd airflow && cp airflow/spark_jars/ /mnt/airflow/scripts/spark_jars/ && sudo docker stack deploy -c docker-compose.yml  airflow-cluster
 
 #Bring up Spark cluster as a Docker Swarm Service
 cd ../spark && sudo docker stack deploy -c docker-compose.yml spark-cluster
