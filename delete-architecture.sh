bf4365babb6e071839bcf5fa394effeaadf5df38
Vasista Polali <vasistabhargava@gmail.com>
2023-07-11 17:08:39 +0200

    update_airflow_config

--- delete-architecture.sh
+++ delete-architecture.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
 
+echo "Delete Airflow cluster"
 docker stack rm airflow-cluster
 
+echo "Delete Spark cluster"
 docker stack rm spark-cluster
 
+echo "Delete Kafka cluster"
 docker stack rm kafka-cluster
 
+echo "Remove overlay network"
 docker network remove data-stream
 
+echo "Leave Swarm mode"
 docker swarm leave -f
+
+echo "Delete mount directory"
 rm -r /mnt
--- deploy-architecture.sh
+++ deploy-architecture.sh
@@ -1,35 +1,42 @@
 #!/usr/bin/env bash
 
 
-#Initialize single node docker swarm cluster
+echo "Initialize docker swarm cluster"
 docker swarm init
 
-#Create overlay network
+echo "Create overlay network"
 docker network create -d overlay --scope=swarm data-stream
 
-#Create local volumes for data persistence
+echo "Create local dir for data persistence"
 mkdir -p /mnt/airflow/dags /mnt/airflow/logs /mnt/airflow/config /mnt/airflow/plugins /mnt/airflow/scripts /mnt/data /mnt/kafka-1-data /mnt/spark_checkpoint/
 chmod -R 775 /mnt
 
+
+echo "Download Apache Spark binaries"
 wget https://dlcdn.apache.org/spark/spark-3.4.1/spark-3.4.1-bin-hadoop3.tgz
 tar -xvf spark-3.4.1-bin-hadoop3.tgz
 mv spark-3.4.1-bin-hadoop3 /mnt/airflow/scripts/spark
 rm spark-3.4.1-bin-hadoop3.tgz
 
-cp spark/jars/* /mnt/airflow/scripts/spark/jars/
+
+echo "Copy additional Spark jars"
+cp airflow/spark_jars/ /mnt/airflow/scripts/spark_jars/
 cp airflow/dags/* /mnt/airflow/dags/
 cp application/*  /mnt/airflow/scripts/
 
-#Bring up Airflow cluster as a Docker Swarm Service
+echo "Bootstrap Airflow cluster as a Docker Swarm Service"
+
 cd airflow && sudo docker stack deploy -c docker-compose.yml  airflow-cluster
 chmod -R 775 /mnt/airflow/scripts/spark_jars/
 
-#Bring up Spark cluster as a Docker Swarm Service
+echo "Bootstrap Spark cluster as a Docker Swarm Service"
 cd ../spark && sudo docker stack deploy -c docker-compose.yml spark-cluster
 
-#Bring up Kafka cluster as a Docker Swarm Service
+echo "Bootstrap Kafka cluster as a Docker Swarm Service"
 cd ../kafka && sudo docker stack deploy -c docker-compose.yml kafka-cluster
 
+
+# Web UI URL's for tthe services
 
 #airflow ui: http://<host>:8080
 
