f63dbb0dc958e2d90169eeed6f89fcd5d4f781bb
Vasista Polali <vasistabhargava@gmail.com>
2023-07-11 14:26:30 +0200

    update_streaming_dag

--- airflow/dags/weather_streaming_dag.py
+++ airflow/dags/weather_streaming_dag.py
@@ -19,9 +19,9 @@ with DAG(
 ) as dag:
     submit_job = BashOperator(
         task_id="spark_submit_image_streaming",
-        bash_command='${AIRFLOW_HOME}/spark/bin/spark-submit --jars /usr/local/airflow/scripts/spark_jars/commons-pool2-2.11.1.jar,/usr/local/airflow/scripts/spark_jars/spark-sql-kafka-0-10_2.12-3.4.0.jar,'
-                     '/usr/local/airflow/scripts/spark_jars/kafka-clients-3.4.0.jar,/usr/local/airflow/scripts/spark_jars/spark-token-provider-kafka-0-10_2.12-3.4.0.jar --master spark://spark:7077 '
-                     '--name image-streaming /usr/local/airflow/scripts/main_weather.py kafka-1:9092  weather_data weather_data_avg {{ execution_date }}')
+        bash_command='${AIRFLOW_HOME}/scripts/spark/bin/spark-submit --jars ${AIRFLOW_HOME}/scripts/spark_jars/commons-pool2-2.11.1.jar,${AIRFLOW_HOME}/scripts/spark_jars/spark-sql-kafka-0-10_2.12-3.4.0.jar,'
+                     '/${AIRFLOW_HOME}/scripts/spark_jars/kafka-clients-3.4.0.jar,${AIRFLOW_HOME}/scripts/spark_jars/spark-token-provider-kafka-0-10_2.12-3.4.0.jar --master spark://spark:7077 '
+                     '--name image-streaming ${AIRFLOW_HOME}/scripts/weather_streaming.py kafka-1:9092  weather_data weather_data_avg {{ execution_date }}')
 
 
 submit_job
